482c7395fb400e2263f1fa30dc94d694a82fbbbd
AnonMiraj <nabilmalek48@gmail.com>
2023-08-16 18:41:54 +0000

    idk

--- .config/nvim/lua/plugins/modules/competitest.lua
+++ .config/nvim/lua/plugins/modules/competitest.lua
@@ -1,6 +1,6 @@
 local M =
 {
-  "xeluxee/competitest.nvim",
+  "AnonMiraj/competitest.nvim",
   --a Neovim plugin to automate testcases management and checking for Competitive Programming
   ft = "cpp",
   -- cmd = "Competitest",
@@ -29,7 +29,20 @@ end
 
 function M.config()
   require('competitest').setup {
+    -- received_problems_path = "$(HOME)/library/Competitive Programming/$(JUDGE)/$(CONTEST)/$(PROBLEM).$(FEXT)",
+    function(task, file_extension)
+      local hyphen = string.find(task.group, " - ")
+      local judge, contest
+      if not hyphen then
+        judge = task.group
+        contest = "unknown_contest"
+      else
+        judge = string.sub(task.group, 1, hyphen - 1)
+        contest = string.sub(task.group, hyphen + 3)
+      end
+    end,
     received_problems_path = "$(HOME)/library/Competitive Programming/$(JUDGE)/$(CONTEST)/$(PROBLEM).$(FEXT)",
+
     received_contests_directory = "$(HOME)/library/Competitive Programming/$(JUDGE)/$(CONTEST)",
     received_contests_problems_path = "$(PROBLEM).$(FEXT)",
     received_problems_prompt_path = false,
@@ -37,6 +50,7 @@ function M.config()
     evaluate_template_modifiers = true,
     received_contests_prompt_directory = false,
     received_contests_prompt_extension = false,
+    open_received_contests = false,
     received_files_extension = "cpp",
     template_file = {
       cpp = "~/.config/nvim/template/CPP.cpp",
